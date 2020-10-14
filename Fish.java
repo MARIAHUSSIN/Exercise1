4815c530836391fba903320f08e6590668fc65a0
MARIAHUSSIN <72845266+MARIAHUSSIN@users.noreply.github.com>
2020-10-14 17:26:25 +0800

    Add files via upload

--- Fish.java
+++ Fish.java
@@ -1,10 +1,20 @@
 //Author:Maria Binti Abu Hussin
 public class Fish {
 	//Atribute or data member or fields
-	public String color="blue";  //default value
+	private String color = "blue";  //default value
 
 	//Method
 	public void Swim(){
 		System.out.println(color+" fish swim");
+	}
+
+	//Setter/Mutator method
+	public void SetColor(String warna) {
+		color=warna;
+	}
+
+	//Getter/Accessor method
+	public String GetColor(){
+		return color;
 	}
 }
